8d8da94902680c274c5f5b3ea9a8aa318f8f0583
Aisheair <ankitkargupta98313@gmail.com>
2025-01-25 18:08:02 +0530

    card componenets 2

---
+++ card-components/src/Card.jsx
@@ -0,0 +1,14 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+        <img src="https://i.pinimg.com/736x/de/7a/1f/de7a1f043528e1e2df8327f2df81a8e6.jpg" alt="profile picture" width={150} height={150} />
+        <h1>Aisheair</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+    )
+
+}
+
+export default Card
