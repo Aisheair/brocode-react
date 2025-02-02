1933e7038421b9b4a5419fe883ff26b18f9e9a71
Aisheair <ankitkargupta98313@gmail.com>
2025-02-02 18:05:19 +0530

    d

---
+++ userState-hook/src/App.jsx
@@ -0,0 +1,19 @@
+// React hook = Special function that allows functional components
+//              to use React features without writing class components
+//(useState, useEffect, useContext, userReducer, userCallback, and more..)
+
+// userState() = A Reaxt Hook that allows the creation of a stateful variable
+//               AND a setter function to update its value in the Virtual DOM
+//               [name, setName]
+
+
+
+function App() {
+
+
+  return (
+ <></>
+  )
+}
+
+export default App
